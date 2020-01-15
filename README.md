d97d7b28d3d6d2bf241462693d11c80af453b4b1
carlabeltran <carlabeltran14@icloud.com>
2020-01-14 18:28:23 -0600

    updated read me file added comments fully functional generate button and copy to clipboard button

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# -03-Password-Gen
+Random Password Generator
+
+## Description
+The random password generator application generates a strong password based on user-selected criteria.
+
+## Purpose
+For companies that handle large amounts of sensitive data, weak passwords can pose a real security threat. An application that can generate strong passwords quickly and effortlessly saves employees time and ensures secure access to data. As an employee with access to sensitive data to be have the ability to create a strong random password that meets certain criteria effortlessly provides greater security and productivity.
+
+## Acceptance Criteria
+The user will choose from the following password criteria:
+
+  Length 
+    -(must be between 8 and 128 characters)
+
+  Character types:
+    -Special characters (see examples)
+    -Numeric characters
+    -Lowercase characters
+    -Uppercase characters
+
+## Features
+    -The random password generator application has a clean and polished user interface and is responsive, ensuring that it adapts to multiple screen sizes.
+
+    --The random password generator application validates user input and ensures that at least one character type is selected. 
+
+    -The random password generator application validates user input and ensures that length requirement is met.
+
+    -The random password generator application lists all previously generated passwords for the session under previously generated passwords for the user to view. 
+    
+    -The random password generator application gives the user the option to click a button to copy the password to their clipboard.
+
+## Installation 
+No installation required. Just visit the link.
+(The URL of the deployed application)
+
+## Usage
+Random password generator user interface.
+![Image description](link-to-image)
+
+Input password length (min 8 max 128)
+![Image description](link-to-image)
+
+View previously generated passwords below
+![Image description](previous)
+
+## Credits
+
+
+## License
+
+
+## Tests
+
+--
+Carla Beltran 2019
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,23 +1,35 @@
-//DOM Elements
-const resultEl = document.getElementById('result');
-console.log('result');
-const lengthEl = document.getElementById('length');
-console.log('length');
-const uppercaseEl = document.getElementById('uppercase');
-console.log('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-console.log('lowercase');
-const numbersEl = document.getElementById('numbers');
-console.log('numbers');
-const symbolsEl = document.getElementById('symbols');
-console.log('symbols');
-const generateEl = document.getElementById('generate');
-console.log('generate');
-const clipboard = document.getElementById('clipboard');
-console.log('clipboard');
-const previousPasswords = document.getElementById('lastNums');
-console.log('lastNums');
+//DOM(Document Object Model)
+const resultEl = document.getElementById("display");
+console.log("display");
+
+const lengthEl = document.getElementById("length");
+console.log("length");
+
+const uppercaseEl = document.getElementById("uppercase");
+console.log("uppercase");
+
+const lowercaseEl = document.getElementById("lowercase");
+console.log("lowercase");
+
+const numbersEl = document.getElementById("numbers");
+console.log("numbers");
+
+const symbolsEl = document.getElementById("symbols");
+console.log("symbols");
+
+const generateEl = document.getElementById("generate");
+console.log("generate");
+
+const clipboardEl = document.getElementById("clipboard");
+console.log("clipboard");
+
+const previousPasswords = document.getElementById("lastNums");
+console.log("lastNums");
+
+//object for random functions
 const randomFunc = {
+	//properties of my object
+	//key: property
     lower: getRandomLower,
     upper: getRandomUpper,
     number: getRandomNumber,
@@ -27,76 +39,127 @@ console.log(randomFunc.lower);
 console.log(randomFunc.upper);
 console.log(randomFunc.number);
 console.log(randomFunc.symbol);
-//generate event listener
+
+//copy clipboard event listener
+clipboardEl.addEventListener('click', () => {
+	
+	//creating a element from within javascript
+	const textarea = document.createElement('textarea');
+	
+	//we variable and get from result innerText 
+	const password = resultEl.innerText;
+	
+	//if click copy to clipboard and no value in pwd just return
+	if(!password) {
+	
+		// takes text area and put value of password into it
+		textarea.value = password;
+		
+		//places textarea in the body
+		document.body.appendChild(textarea);
+		//calling select
+		//textarea.select();
+		textarea.select();
+		
+		//copys the content to clipboard
+		document.execCommand("copy");
+		
+		//removes the text area from the document
+		textarea.remove();
+		
+		// alert message
+		alert("Password copied to clipboard!");
+	}
+});
+
+//generate event listener(this is just getting the values)
 generate.addEventListener('click', () => {
+	//this has the value of length
 	const length = +lengthEl.value;
-	console.log(typeof length);
+	//console.log(length)
     const hasLower = lowercaseEl.checked;
     const hasUpper = uppercaseEl.checked;
     const hasNumber = numbersEl.checked;
 	const hasSymbol = symbolsEl.checked;
-	
+	//console.log(typeof length);
+	//console.log(hasLower, hasUpper, hasNumber, hasSymbol);
 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
 });
-//console.log(hasLower, hasUpper, hasNumber, hasSymbol);
-//generate password
+
+//generate password function
 function generatePassword(lower, upper, number, symbol, length) {
+	
+	//initate password variable
 	let generatedPassword = '';
+
+	//allows count array the number of checked items
 	const typesCount = lower + upper + number + symbol;
 	console.log('typesCount: ', typesCount);
-	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+	//array of objects has keys
+	const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
+	//filter out what is false=unchecked types in the array and looping through based on a true or false value
 	console.log('typesArr: ', typesArr);
-    if(typesCount === 0) {
-        return '';
+	
+	//validating non-checked not to proceed(typesCount) = the number of checked boxes 
+	if(typesCount === 0) {
+		
+		//if types count is 0 we want to return nothing.
+		return '';
 	}
-    for(let i=0; i<length; i+=typesCount) {
-        typesArr.forEach(type => {
+	
+	// for loop over the length and then call the generator function for each type 
+	// (let i=0; condition-if i is less than the length"20"; increment by the types count the number of checked boxes)
+    for(let i = 0; i < length; i += typesCount) {
+		
+		//loop through array.with-forEach(each type)
+		typesArr.forEach(type => {
+			
+			//open block to get function name = object.keys and pass in type because looping through types array and get first value
 			const funcName = Object.keys(type)[0];
 			console.log('funcName: ', funcName);
+			
+			//appending empty generated password string
 			generatedPassword += randomFunc[funcName]();
 		});
-	};
-	document.getElementById('display').value = generatedPassword;
-	document.getElementById('lastNums').innerHTML += generatedPassword + "<br />";
-	//set default length display of 25
-	document.getElementById("length").innerHTML = "Length: 25";
-	//function to set length based on slider position
-	document.getElementById("length").oninput = function() {
-    if(document.getElementById("length").value > 0){
-		alert
-		document.getElementById("length").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "Length: 8";
-    }
-	const finalPassword = generatedPassword.slice(0, length);
-	console.log(generatedPassword.slice(0, length));
+	}
+	//This validates the length and password length match
+	const finalPassword = generatedPassword.slice(0, length);	
+	//console.log(generatedPassword.slice(0, length));
+	//console.log(generatedPasword);
+	
+	//display generated password
+	document.getElementById('display').value = finalPassword;
+
+	//display previously generated passwords
+	document.getElementById('lastNums').innerHTML += finalPassword + "<br />"; 
+
 	return finalPassword;
-	};
-}	
+}
+
+//generator functions charset website: http://www.net-comber.com/charset.net
+//random lowercase letters
 function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 console.log(getRandomLower());
 
+//random uppercase letters 
 function getRandomUpper() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 console.log(getRandomUpper());
 
+//random number 
 function getRandomNumber() {
     return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 console.log(getRandomNumber());
 
+//random symbols 
 function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.'
     return symbols[Math.floor(Math.random() * symbols.length)];
 }
 console.log(getRandomSymbol());
 
-function copyPassword() {
-    document.getElementById("display").select();
-    document.execCommand("copyClipboard");
-    alert("Password copied to clipboard!");
-}
